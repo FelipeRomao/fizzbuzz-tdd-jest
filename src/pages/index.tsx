9345eec7dee3a7db07a565ae02bc48750185dbc6
Felipe Romao <feliperomao.a@gmail.com>
2021-02-19 12:53:21 -0300

    Simple TDD example with Jest and typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+export default function fizzbuzz(numero) {
+  let resultado = '';
+  if (numero % 3 == 0) {
+    resultado = "Fizz";
+  }
+
+  if (numero % 5 == 0) {
+    resultado += "Buzz";
+  }
+  if (resultado === "") {
+    resultado = numero;
+  }
+  return resultado;
+}
